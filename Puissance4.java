12da1d5437db2b0bc0a4c187e59e17af102abf14
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-11-17 09:07:35 +0100

    Update Puissance4.java

--- Puissance4.java
+++ Puissance4.java
@@ -8,12 +8,34 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class projet {
+	public static int[][] grille = new int[6][7];
+	
 	public static int entierAleatoire(int a, int b){
 		//Retourne un entier aléatoire entre a (inclus) et b (inclus)
 		return ThreadLocalRandom.current().nextInt(a, b + 1);	
 	}
-
+	
+	public static void jouer(int joueur, int colonne) {
+		for(int i = 0; i < grille.length; i ++) {
+			if(grille[i][colonne] == 0) {
+				grille[i][colonne] = joueur;
+				return;
+			}
+		}
+	}
+	public static void afficheTab2D(int [][] tab) {
+		String rep = "{";
+		for(int i =0; i < tab.length; i ++) {
+			rep += "{";
+			for(int j = 0; j < tab[i].length-1; j ++) {
+				rep += tab[i][j]+ ", ";
+			}
+			rep += tab[i][tab[i].length-1] + "},\n";
+		}
+		System.out.println(rep +"}");
+	}
 	public static void main(String[] args) {
-		int[][] grille = new int[6][7];
+		jouer(1, 3);
+		afficheTab2D(grille);
 	}
 }

41646593cc3c5e7a510544967e1817e268443e26
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-11-17 09:53:27 +0100

    Update Puissance4.java

--- Puissance4.java
+++ Puissance4.java
@@ -23,6 +23,39 @@ public class projet {
 			}
 		}
 	}
+	public static int[][] symetrieHorizontale(int[][] tab){
+		int n = tab.length;
+		int[][] rep = new int[n][];
+		
+		for(int i =0; i < n; i ++) {
+			rep[i] = new int[tab[i].length];
+			for(int j = 0; j < tab[i].length; j ++) {
+				rep[i][j] = 0;
+			}
+		}
+		for(int i = 0; i < n; i ++) {
+		
+			for(int j = 0; j < tab[i].length; j ++) {
+
+				if(n%2 == 0) {
+					if(i <= n /2) {rep[(n-1) - i][j] = tab[i][j];}
+					else {
+						if(i != tab.length-1){rep[i - n/2][j] = tab[i][j];}
+						else {rep[0][j] = tab[i][j];}
+					}
+				}
+				else {
+					if(i != n/2) {
+						if(i <= n /2) {rep[(n-1) - i] [j] = tab[i][j];}
+						else {rep[i - n/2-1][j] = tab[i][j];}
+					}
+					else {rep[i][j] = tab[i][j];}
+				}
+			}
+		}
+		return rep;
+	}
+	//FOCTION TEST
 	public static void afficheTab2D(int [][] tab) {
 		String rep = "{";
 		for(int i =0; i < tab.length; i ++) {
@@ -34,8 +67,32 @@ public class projet {
 		}
 		System.out.println(rep +"}");
 	}
+	//FONCTION TEST
+	public static void afficheGrille() {
+		String rep = "";
+		int[][] tab = symetrieHorizontale(grille);
+		for(int i = 0; i < tab.length; i++) {
+			rep+="|";
+			for(int j = 0; j < tab[i].length; j ++) {
+				if(tab[i][j] == 0) {rep+=" |";}
+				else if(tab[i][j] == 1) {rep+="X|";}
+				else if(tab[i][j] == 2) {rep+="O|";}
+			}
+			rep+="\n";
+		}
+		System.out.print(rep);
+	}
+	
 	public static void main(String[] args) {
 		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(2,4);
 		afficheTab2D(grille);
+		afficheTab2D(symetrieHorizontale(grille));
+		afficheGrille();
 	}
 }

11a2e7eacd29cc4c5068cd86ff4b97b8fdcb781c
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-11-30 17:37:56 +0100

    Update IA.java

--- IA.java
+++ IA.java
@@ -6,12 +6,12 @@ public class IA {
     public static boolean detect_win_condition(int player)
     {
 
-        for (int i = 0; i < Puissance4.repre.grille.length; i++)
-            for (int j = 0; j < Puissance4.repre.grille[i].length; j++)
+        for (int i = 0; i < repre.grille.length; i++)
+            for (int j = 0; j < repre.grille[i].length; j++)
                 {
                     //check line/diag/column
-                    if (Puissance4.repre.grille[i][j] == player) {
-                        if (check_line(Puissance4.repre.grille[i], i, player) || check_column(Puissance4.grille, i, j, player) || check_diagonal(Puissance4.grille, i, j, player))
+                    if (repre.grille[i][j] == player) {
+                        if (check_line(repre.grille[i], i, player) || check_column(repre.grille, i, j, player) || check_diagonal(repre.grille, i, j, player))
                             return (true);
                     }
                 }

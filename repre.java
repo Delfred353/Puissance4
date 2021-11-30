7434e5728d354d95a9b2e1b71ab9ee6739b0419d
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-11-30 16:59:24 +0100

    Update repre.java

--- repre.java
+++ repre.java
@@ -13,13 +13,37 @@ public class repre{
 	public static void jouer(int joueur, int colonne) {//1<=joueur<=2
 		//Place un jeton dans la colonne indiqué dans un emplacement libre, en partant du haut de la colonne
 		for(int i = 0; i < grille.length; i ++) {
-			if(grille[i][colonne] == 0) { //Test rajouter comme sécurité, 0 est une case vide
+			if(grille[i][colonne] == 0) {
 				grille[i][colonne] = joueur;
 				return;
 			}
-			else{System.out.println("Colonne pleine, votre tour a été passé");}//Si la colonne est pleine, le coup du joeur n'est pas compatibilisé
 		}
 	}
 	//Exercice 2
 	
+	//tests
+	
+	public static void afficheTab2D(int [][] tab) {
+		String rep = "{";
+		for(int i =0; i < tab.length; i ++) {
+			rep += "{";
+			for(int j = 0; j < tab[i].length-1; j ++) {
+				rep += tab[i][j]+ ", ";
+			}
+			rep += tab[i][tab[i].length-1] + "},\n";
+		}
+		System.out.println(rep +"}");
+	}
+	
+	public static void main(String[] args) {
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(1, 3);
+		jouer(2,4);
+		afficheTab2D(grille);
+	}
+	//tests
 }

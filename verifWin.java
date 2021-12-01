1ac1dbaee71f61eb24762798f55aabd0d19c9369
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-12-01 11:23:21 +0100

    this file contain the old code off the IA file.

---
+++ verifWin.java
@@ -0,0 +1,78 @@
+package Puissance4;
+
+public class verifWin{
+	private static int to_win = 3;
+	
+	  public static boolean detect_win_condition(int player)
+	    {
+
+	        for (int i = 0; i < repre.grille.length; i++)
+	            for (int j = 0; j < repre.grille[i].length; j++)
+	                {
+	                    //check line/diag/column
+	                    if (repre.grille[i][j] == player) {
+	                        if (check_line(repre.grille[i], i, player) || check_column(repre.grille, i, j, player) || check_diagonal(repre.grille, i, j, player))
+	                            return (true);
+	                    }
+	                }
+	        return (false);
+	    }
+	  
+	  
+	  
+	  
+	  
+
+	    public static boolean check_line(int [] tab, int index, int player)
+	    { 
+	        for (int i = index; i < to_win; i++)
+	            if (i >= tab.length || tab[i] != player)
+	                return (false);
+	        return (true);
+	    }
+	    // A refaire
+	    public static boolean check_diagonal(int[][] tab, int i, int j, int player)
+	    {
+	        int cpyi = i, cpyj = j;
+
+	        if (i > to_win && j > to_win)
+	            {
+	                //regarde bas a droite
+	                while (cpyi < to_win && tab[cpyi][cpyj] == player)
+	                    {
+	                        cpyi++;
+	                        cpyj++;
+	                    }
+	                if (tab[cpyi][cpyj] == player)
+	                    return (true);
+	            }
+	        cpyi = i;
+	        cpyj = j;
+	        if (i < to_win && j < to_win)
+	            {
+	                //regarde en bas a gauche
+	                while (cpyi < to_win && tab[cpyi][cpyj] == player)
+	                {
+	                    cpyi--;
+	                    cpyj--;
+	                }
+	                if (tab[cpyi][cpyj] == player)
+	                    return (true);
+	            }
+	        return (false);
+	    }
+	    //A refaire
+
+	    public static boolean check_column(int[][] tab, int i, int j, int player)
+	    {
+	        //regarde en bas
+	        if (i < to_win)
+	            {
+	                while (i <= to_win && tab[i][j] == player)
+	                    i++;
+	                if (tab[i][j] == player)
+	                    return (true);
+	            }
+	        return (false);
+	    }
+}

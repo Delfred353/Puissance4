8512b9a2914743b79fb5dee0ce0020170fd01d29
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-12-01 12:02:39 +0100

    Update IA.java

--- IA.java
+++ IA.java
@@ -1,1 +1,36 @@
+package Puissance4;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+public class IA{
+	
+	static int joueur;
+	
+	public IA(int j) {
+		joueur = j;
+	}
+	
+	
+	public static int entierAleatoire(int a, int b){
+		//Retourne un entier aléatoire entre a (inclus) et b (inclus)
+		return ThreadLocalRandom.current().nextInt(a, b + 1);	
+	}
+	
+	public static boolean colonneEstLibre(int c) {
+		if(repre.grille[6][c] == 0) {return true;}
+		return false;
+	}
+	
+	public static void joueCoupRandom() {
+		boolean joue = false;
+		int c = 0;
+		while (joue != true){
+			c = entierAleatoire(0,6);
+			if(colonneEstLibre(c)) {
+				joue = true;
+			}
+		}
+		repre.jouer(joueur,  c);
+	}
+
+}

c48a8e04308e254ce442c1b8cb2640f902d95a16
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-12-16 20:40:54 +0100

    Update LaunchPuissance4.java

--- LaunchPuissance4.java
+++ LaunchPuissance4.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //File Version : 0.4
 //-----------------------------------------------------
 
-class LaunchPuissance4{
+public class LaunchPuissance4{
 
 	public static void boucleDeJeu() {
 		
@@ -16,11 +16,12 @@ class LaunchPuissance4{
 		int grillePleine = 0;
 		int jouant = 1;
 		Scanner sc = new Scanner(System.in);//Ouvre une lecture de l'entrée standard pour les coups des joueurs.
-		while(victoire == 0 || grillePleine != 0) {
+		
+		while(victoire == 0 && grillePleine == 0) {
 			Display.afficheGrille();
 			System.out.println("Quel coup pour le joueur " + jouant);
 			int c = sc.nextInt();
-			repre.jouer(jouant, c);
+			repre.jouer(jouant, c, repre.grille);
 			
 			if(verifWin.detect_win_condition(jouant)) {victoire = jouant;}
 			else if(verifWin.grilleEstPleine()) {grillePleine = 1;}
@@ -33,16 +34,8 @@ class LaunchPuissance4{
 		sc.close();
 		if(victoire == 1) {System.out.println("Victoire du joueur 1");}
 		else if(victoire == 2) {System.out.println("Victoire du joueur 2");}
-		else if(grillePleine == 1) {System.out.println("Egalité des deux joueurs ( la grille est pleine)");}
+		else if(grillePleine == 1) {System.out.println("Égalité des deux joueurs ( la grille est pleine)");}
 	}
-
-	public static void jeu() {
-		//Lance le jeu
-		repre.initialiseGrille();
-		boucleDeJeu();
-		Display.afficheGrille();
-	}
-}
 
 	public static void jeu() {
 		//Lance le jeu

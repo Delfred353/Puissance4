227d8047c263c471c1466b26d4e2cb60704426df
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2021-12-17 23:51:57 +0100

    Update verifWin.java

--- verifWin.java
+++ verifWin.java
@@ -1,113 +1,64 @@
+package Puissance4;
 
-public class verifWin{
+import java.util.concurrent.ThreadLocalRandom;
+
+public class IA{
 	
-	  public static boolean win(int player, int [][] grille)
-	    {
-
-	        for (int i = 0; i < grille.length; i++)
-	            for (int j = 0; j < grille[i].length; j++)
-	                {
-	                    //check line/diag/column
-	                    if (grille[i][j] == player) {
-	                        if (checkLine(i, player, grille) || checkColumn(j, player, grille) || checkDiag(j, player, grille))
-	                            return (true);
-	                    }
-	                }
-	        return (false);
-	    }
-
-	  
-
-	   
-
-	    public static boolean checkColumn(int j, int jouant, int [][] grille) {
-	    	int k = 0;
-	    	for(int i = 0; i < grille.length; i++) {
-	    		if(grille[i][j] == jouant) {
-	    			k+=1; 
-	    			if(k==4) {return true;}
-	    		}
-	    		else {k=0;}
-	    		
-	    	}
-	    	return false;
-	    }
-	    
-	   //NE PAS TOUCHER 
-	    public static boolean grilleEstPleine() {
-			//Détecte si la dernière ligne du tableau(donc le haut des colonnes) est plein, si c'est le cas return true sinon false
-			for(int j = 0; j < repre.grille[5].length ; j ++) {
-				if(repre.grille[5][j] == 0) {return false;}
+	static int[][] CopieGrille = new int [6][7];
+	static int joueur;
+	
+	public IA(int j) {
+		joueur = j;
+	}
+	
+	
+	public static int entierAleatoire(int a, int b){
+		//Retourne un entier aléatoire entre a (inclus) et b (inclus)
+		return ThreadLocalRandom.current().nextInt(a, b + 1);	
+	}
+	
+	public static boolean colonneEstLibre(int c) {
+		if(repre.grille[6][c] == 0) {return true;}
+		return false;
+	}
+	
+	public static void joueCoupRandom() {
+		boolean joue = false;
+		int c = 0;
+		while (joue != true){
+			c = entierAleatoire(0,6);
+			if(colonneEstLibre(c)) {
+				joue = true;
 			}
-			return true;
 		}
-	    //NE PAS TOUCHER
-	    
-	    public static boolean checkLine(int i, int jouant, int [][] grille) {
-	    	int k = 0;
-	    	for(int j = 0; j < grille[i].length; j ++) {
-	    		if(grille[i][j] == jouant) {k+=1;}
-	    		else {k = 0;}
-	    	}
-	    	if(k == 4) {return true;}
-	    	return false;
-	    }
-	    
-	    public static boolean checkDiag(int i, int jouant, int [][]grille) {
-	    	int j = i;
-	    	int k = 0;
-	    	if(j < grille[0].length) {
-		    	for(int x = 0; x < grille.length; x++) {
-		    		if(grille[x][j] == jouant) {k +=1;
-		    		
-		    		if(k == 4) {return true;}}
-		    		else {k=0;
-		    			
-		    		}
-		    		if(j+1 < grille[x].length) {j+=1;}
-		    	}
-		    	j = i;
-	    	}
-		    
-	    	if(i-1 != 0) {
-	    		for(int x = 0; x < grille.length; x++) {
-		    		if(grille[x][j] == jouant) {k +=1;if(k ==4) {return true;}}
-		    		else {k=0;}
-		    		if(j-1 >= 0) {j-=1;}
-		    		else {return false;}
-		    	}
-	    	}
-	    	return false;  
-	    }
-	    
-	    /*
-	    public static void main(String[] args) {
-		    //Faire des tests clairs
-	    	System.out.println(checkLine(1, 1));
-	    	jouer(1, 3, grille);
-	    	jouer(1, 3, grille);
-	    	jouer(1, 3, grille);
-	    	jouer(1, 3, grille);
-	    	jouer(1, 4, grille);
-	    	jouer(1, 4, grille);
-	    	jouer(1, 4, grille);
-	    	jouer(1, 4, grille);
-	    	jouer(1, 5, grille);
-	    	jouer(1, 5, grille);
-	    	jouer(1, 5, grille);
-	    	jouer(1, 5, grille);
-	    	jouer(1, 6, grille);
-	    	
-	    
-	    	System.out.println(checkLine(0, 1));
-	    	Display.afficheGrille();
-	    	System.out.println(checkDiag(6, 1));
-	    	System.out.println(win(1));
-	    }
-	    */
-	    
-	    
-	    
-	    
-	    
+		repre.jouer(joueur,  c, repre.grille);
+	}
+	
+	public static void copieGrille(){
+		for(int i = 0; i < CopieGrille.length; i++) {
+			for(int j = 0; j < CopieGrille[0].length; j ++) {
+				CopieGrille[i][j] = repre.grille[i][j];
+			}
+		}
+
+	}
+	
+	public static void reinitialiserCopieGrille(){
+		copieGrille();
+	}
+	
+	public static void peutGagner1Coup() {
+		copieGrille();
+	
+		for(int j = 0; j < CopieGrille[0].length; j++) {
+			repre.jouer(joueur, j, CopieGrille);
+			if (verifWin.win(joueur, CopieGrille)) { 
+				repre.jouer(joueur, j, repre.grille);
+				return;
+			}
+		}
+		joueCoupRandom();
+		
+	}
+
 }

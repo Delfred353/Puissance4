cf9a1ede4c0eeea58f97769e07c0896668bebb20
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2022-03-02 10:33:10 +0100

    Implementation of an playable AI

--- puissance4.java
+++ puissance4.java
@@ -1,16 +1,27 @@
 ///----------------------------------------------------
 // Auteurs : Pierre Roumane, Nicolas Merrer
-// Version : 0.78 
+// Game Version : 0.95
 //----------------------------------------------------
 
 
 package Puissance4;
 
+import java.util.Scanner;
 
 public class puissance4 {
 	
 	public static void main(String[] args) {
-		LaunchPuissance4.jeu();
+		System.out.println("Voulez-vous contre l'ia ? 1 pour oui et 0 pour non");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		if( x == 1){
+			LaunchPuissance4.jeuIA();	
+		}
+		else {
+		LaunchPuissance4.jeu();}
+		sc.close();
 	}
+	
 	
 }
